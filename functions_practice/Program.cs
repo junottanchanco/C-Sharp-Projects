20a4464b3702f6634bf33f4a951c3154bc2ba717
Jesus Tanchanco <junot_t@yahoo.com>
2017-10-04 10:57:09 -0400

    How to use functions

---
+++ functions_practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace functions_practice
+{
+    class Program
+    {
+        // public static void SayHello(string name = "buddy"){
+        public static string SayHello(string name = "buddy"){
+            // Console.WriteLine("Hello {0}, how are you doing today?", name);
+            // Console.WriteLine("Hey " + name);
+            return $"Hey {name}";
+        }
+        public static void Main(string[] args)
+        {
+            string greeting;
+            greeting = SayHello();
+            Console.WriteLine(greeting);
+            // SayHello("Junot");
+        }
+    }
+}
